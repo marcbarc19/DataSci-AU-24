c6d3753ba117f0a2d173f33936fdba2f94b8351b
marcbarc19 <marc19barcelos@gmail.com>
2024-02-09 16:18:54 +0100

    sdf

---
+++ setup_git.sh
@@ -0,0 +1,8 @@
+# run using 
+# bash path_to_folder/setup_git.sh
+
+git config --global user.email "marc19barcelos@gmail.com"
+git config --global user.name "marcbarc19"
+
+sudo apt-get update
+sudo apt-get install python3-venv
