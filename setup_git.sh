a0952a878fb4b6665e7a2ff5771802781c7c03bf
marcbarc19 <marc19barcelos@gmail.com>
2024-02-09 16:31:33 +0100

    09022024

--- setup_git.sh
+++ setup_git.sh
@@ -6,3 +6,6 @@ git config --global user.name "marcbarc19"
 
 sudo apt-get update
 sudo apt-get install python3-venv
+
+#source /work/venvs/dspf-s24/bin/activate
+#python3 -m ipykernel install --user --name=dspf-s24
